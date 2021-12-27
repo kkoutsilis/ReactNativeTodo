2bc57edaad73679e2d1ebe5f38c1534ebedb192f
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2021-12-27 19:14:46 +0000

    initial setup

---
+++ App.js
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Today's tasks */}
+        <View style={styles.taskWrapper}>
+          <Text style={styles.sectionTitle}>Today's Tasks</Text>
+          <View style={styles.items}>
+            {/* This is where the tasks will go */}
+            <Task text={"test1"}/>
+          </View>
+        </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop:80,
+    paddingHorizontal:20
+  },
+  sectionTitle:{
+    fontSize:24,
+    fontWeight:"bold",
+  },
+  items:{
+    marginTop:30,
+  },
+});
