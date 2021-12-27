77e8271df6f165296ecaafcffeb59deed8cad399
Konstantinos Koutsilis <kkoutsilis@gmail.com>
2021-12-27 20:25:53 +0000

    update colors

--- App.js
+++ App.js
@@ -60,7 +60,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: '#cac4ce',
   },
   taskWrapper: {
     paddingTop:80,
@@ -85,9 +85,9 @@ const styles = StyleSheet.create({
   input:{
     paddingVertical:15,
     paddingHorizontal:15,
-    backgroundColor:"#FFF",
+    backgroundColor:"#f7ece1",
     borderRadius:60,
-    borderColor:"#C0C0C0",
+    borderColor:"#6247aa",
     borderWidth:1,
     width:250,
 
@@ -96,11 +96,11 @@ const styles = StyleSheet.create({
   addWrapper:{
     width:60,
     height:60,
-    backgroundColor:"#FFF",
+    backgroundColor:"#f7ece1",
     borderRadius:60,
     justifyContent:"center",
     alignItems:"center",
-    borderColor:"#C0C0C0",
+    borderColor:"#6247aa",
     borderWidth:1,
   },
   addText:{},
